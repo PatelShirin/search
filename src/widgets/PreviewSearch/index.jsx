7416d4ef140c5280af8f90f0c5ab3e7308ce0938
PatelShirin <patel.shirin@outlook.com>
2026-01-16 18:32:27 -0500

    fix: remove commented-out SVG code.

--- src/widgets/PreviewSearch/index.jsx
+++ src/widgets/PreviewSearch/index.jsx
@@ -72,12 +72,7 @@ export const PreviewSearchComponent = ({ defaultItemsPerPage = 6 }) => {
             tabIndex={0}
             style={{ outline: 'none', background: 'none' }}
           >
-            {/* X icon SVG */}
-              {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
-                <circle cx="10" cy="10" r="9" stroke="currentColor" strokeWidth="2" fill="white" />
-                <path d="M7 7l6 6M13 7l-6 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-              </svg> */}
-              <XMarkIcon className="w-5 h-5" />{" "}
+            <XMarkIcon className="w-5 h-5" />{" "}
           </button>
         )}
       </form>
