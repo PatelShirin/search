9417a59438a67194b00dfca2ab683baec60fbe1b
PatelShirin <patel.shirin@outlook.com>
2026-01-16 10:09:43 -0500

    fix: refactor Header component layout for improved structure and readability

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,15 +9,23 @@ const Header = () => {
   const src = 'https://dam.chartwell.com/m/57a5a0b97a53083a/original/CHARTWELL_Logo.png';
   return (
     <div className="header-outer">
+      <div className="flex items-center">
+        <Link to="/" tabIndex={1} className="mr-6 flex items-center">
+          <img width={150} height={40} src={src} alt="Chartwell Logo" />
+          {/* <Logo /> */}
+        </Link>
+      </div>
       <div className="header-inner">
-        <div className="flex items-center justify-between">
-          <Link to="/" tabIndex={1}>
-            <img width={150} height={10} src={src} alt="Chartwell Logo" />
-            {/* <Logo /> */}
-          </Link>
-          <PreviewSearch rfkId="rfkid_6" defaultItemsPerPage={6} />
-          <DarkmodeSwitch />
-          <LocaleSelector />
+        <div className="flex items-center w-full">
+          {/* Centered search box */}
+          <div className="flex-1 flex justify-center">
+            <PreviewSearch rfkId="rfkid_6" defaultItemsPerPage={6} />
+          </div>
+          {/* Controls on the right */}
+          <div className="flex items-center ml-4">
+            <DarkmodeSwitch />
+            <LocaleSelector />
+          </div>
         </div>
       </div>
     </div>
